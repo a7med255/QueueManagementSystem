cba37144b9a5b70fedb186a1070518f3b2553a03
a7med.mans255 <a7med.mans25@gmail.com>
2026-02-05 00:40:26 +0200

    Enhance authentication and improve code clarity
    - Added alias for SignInResult in AuthController.cs.
    - Updated Swagger security scheme to use Bearer tokens.
    - Included Microsoft.AspNetCore.Identity.EntityFrameworkCore package.
    - Refined average wait time calculation in StatisticsService.cs.

--- QueueManagementSystem.API/Program.cs
+++ QueueManagementSystem.API/Program.cs
@@ -63,7 +63,7 @@ builder.Services.AddSwaggerGen(options =>
         Description = "JWT Authorization header using the Bearer scheme.",
         Name = "Authorization",
         In = ParameterLocation.Header,
-        Type = SecuritySchemeType.ApiKey,
+        Type = SecuritySchemeType.Http,
         Scheme = "Bearer"
     });
     options.AddSecurityRequirement(new OpenApiSecurityRequirement
